44132729b289a01cccb900e0d87d8958def1b942
dkfranc <dkfranc@ymail.com>
2016-06-09 23:18:04 +0530

    graph color change

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -54,7 +54,7 @@ WHERE `SQ`.`survey_id` = (SELECT `SU`.`id` FROM `survey` AS `SU`)'));
 		foreach($userColl as $ukey=>$value){
 			$colourArr = array("rgba(179,181,198,0.2)", "rgba(134,194,75,0.2)", "rgba(0,255,0,0.2)");
 			$fillcolor = array("rgba(114,224,13,0.5)","rgba(191,202,182,0.5)","rgba(194,114,201,0.5)");
-			$highlight_fillcolor = array("rgba(114,224,13,1)","rgba(0,150,136,1)","rgba(123,170,202,1)");
+			$highlight_fillcolor = array("rgba(114,224,13,1)","rgba(191,202,182,1)","rgba(194,114,201,1)");
 			$strokecolor = array("rgba(114,224,13,1)","rgba(191,202,182,1)","rgba(194,114,201,1)");
 			$labelsArr = $sgraphDatasetArr = array();
 			
